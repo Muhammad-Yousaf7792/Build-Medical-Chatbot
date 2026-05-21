9fec0c6576ffa871c7b9b12e239a38e4802d4aca
Devil Rana <yousafrao970@gmail.com>
2026-05-21 04:05:07 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.0.1',
+    author='Muhammad Yousaf',
+    packages=find_packages(),
+    author_email='yousafrao970@gmail.com',
+    install_requires=[]
+
+)
